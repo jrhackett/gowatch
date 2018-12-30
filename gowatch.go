51195b9d5447aafab443bc19bc2c4d8fa068f220
Jacob Hackett <jacobrhackett@gmail.com>
2018-12-30 14:51:32 -0500

    starting to add tests

--- gowatch.go
+++ gowatch.go
@@ -1,4 +1,4 @@
-package main
+package gowatch
 
 import (
 	"errors"
--- gowatch_test.go
+++ gowatch_test.go
@@ -1,1 +1,22 @@
-package main_test
+package gowatch_test
+
+import (
+	"testing"
+
+	"github.com/jrhackett/gowatch"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestGowatch(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Gowatch")
+}
+
+var _ = Describe("Gowatch", func() {
+	Describe("NewFileWatcher", func() {
+		It("should return a non-nil FileWatcher", func() {
+			Expect(gowatch.NewFileWatcher("./")).ToNot(BeNil())
+		})
+	})
+})
